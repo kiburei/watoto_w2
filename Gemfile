ea064aefb11b9e6b0e54769b16b1c379e8e83efb
kiburei <akiburei@gmail.com>
2018-02-25 23:38:23 +0300

    full google calender rails intergration

--- Gemfile
+++ Gemfile
@@ -4,8 +4,8 @@ git_source(:github) do |repo_name|
   repo_name = "#{repo_name}/#{repo_name}" unless repo_name.include?("/")
   "https://github.com/#{repo_name}.git"
 end
-
-
+# Google Calender
+gem 'google-api-client', require: 'google/apis/calendar_v3'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.5'
 # Use postgresql as the database for Active Record
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'home#index'
+  get '/redirect', to: 'home#redirect', as: 'redirect'
+  get '/callback', to: 'home#callback', as: 'callback'
+  get '/calendars', to: 'home#calendars', as: 'calendars'
+  get '/events/:calendar_id', to: 'home#events', as: 'events', calendar_id: /[^\/]+/
+  post '/events/:calendar_id', to: 'home#new_event', as: 'new_event', calendar_id: /[^\/]+/
 end
